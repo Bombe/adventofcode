591f7b40f0e3953b10c2c17aecae9447f603cdcd
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-12-03 10:55:12 +0100

    Improve day 3

--- src/y2016/Day3.kt
+++ src/y2016/Day3.kt
@@ -5,36 +5,31 @@ fun main(args: Array<String>) {
 	println(second())
 }
 
-fun first() = getInput(3)
-		.map(String::trim)
-		.map { it.replace("  *".toRegex(), " ") }
-		.map { it.split(" ") }
-		.map { it.map(String::toInt) }
-		.map { it.sorted() }
-		.filter { it[0] + it[1] > it[2] }
-		.count()
+fun first() = countValidTriangles()
 
-fun second() = getInput(3)
+fun second() = countValidTriangles {
+	it.fold(FoldResult(emptyList(), emptyList(), emptyList(), emptyList())) { oldFoldResult, columns ->
+		getNewFoldResult(oldFoldResult, columns[0], columns[1], columns[2])
+	}.results
+}
+
+private fun countValidTriangles(triangleProcessor: (List<List<Int>>) -> List<List<Int>> = { it }): Int = getInput(3)
 		.map(String::trim)
 		.map { it.replace("  *".toRegex(), " ") }
 		.map { it.split(" ") }
 		.map { it.map(String::toInt) }
-		.fold(FoldResult(emptyList(), Triplet<List<Int>>(emptyList(), emptyList(), emptyList()))) { oldFoldResult, columns ->
-			getNewFoldResult(oldFoldResult, columns[0], columns[1], columns[2])
-		}.results
+		.let { triangleProcessor.invoke(it) }
 		.map { it.sorted() }
 		.filter { it[0] + it[1] > it[2] }
 		.count()
 
 fun getNewFoldResult(oldFoldResult: FoldResult, first: Int, second: Int, third: Int): FoldResult =
 		oldFoldResult.run {
-			if (currentTriangle.first.size < 2) {
-				FoldResult(results, Triplet(currentTriangle.first + first, currentTriangle.second + second, currentTriangle.third + third))
+			if (firstColumn.size < 2) {
+				FoldResult(results, firstColumn + first, secondColumn + second, thirdColumn + third)
 			} else {
-				FoldResult(results + listOf(currentTriangle.first + first, currentTriangle.second + second, currentTriangle.third + third), Triplet(emptyList(), emptyList(), emptyList()))
+				FoldResult(results + listOf(firstColumn + first, secondColumn + second, thirdColumn + third), emptyList(), emptyList(), emptyList())
 			}
 		}
 
-data class FoldResult(val results: List<List<Int>>, val currentTriangle: Triplet<List<Int>>)
-
-data class Triplet<out T>(val first: T, val second: T, val third: T)
+data class FoldResult(val results: List<List<Int>>, val firstColumn: List<Int>, val secondColumn: List<Int>, val thirdColumn: List<Int>)
