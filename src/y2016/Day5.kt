eaf708f06314c62ce5be65b9e67fd25dad625734
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-12-05 19:11:47 +0100

    Improve solution of day 5

--- src/y2016/Day5.kt
+++ src/y2016/Day5.kt
@@ -4,48 +4,34 @@ import java.security.MessageDigest
 import javax.xml.bind.DatatypeConverter
 
 fun main(args: Array<String>) {
-	println(first2(getInput().first()))
-	println(second())
+	getInput().first().let {
+		println(first(it))
+		println(second(it))
+	}
 }
 
-private fun first(): String {
-	val input = getInput().first()
-	var index = 0
-	var hashCode = ""
-	while (hashCode.length < 8) {
-		hash(input, index++).let {
-			if (it.startsWith("00000")) {
-				hashCode += it.substring(5..5)
-			}
-		}
-	}
-	return hashCode
-}
+private fun first(input: String) = generateSequence(0, { it + 1 })
+		.map { hash(input, it) }
+		.filter { it.startsWith("00000") }
+		.map { it.substring(5..5) }
+		.take(8)
+		.joinToString("")
 
-private fun second() : String{
-	val input = getInput().first()
-	var index = 0
-	var hashCode = "________"
-	while ("_" in hashCode) {
-		hash(input, index++).let {
-			if (it.startsWith("00000")) {
-				val position = it.substring(5..5).parseHex()
-				if ((position < 8) && (hashCode[position] == '_')) {
-					hashCode = hashCode.substring(0..(position - 1)) + it.substring(6..6) + hashCode.substring(position + 1)
-				}
-			}
-		}
-	}
-	return hashCode
-}
+private fun second(input: String) = generateSequence(0, { it + 1 })
+		.map { hash(input, it) }
+		.filter { it.startsWith("00000") }
+		.map { it.substring(5..5).parseHex() to it.substring(6..6) }
+		.filter { it.first < 8 }
+		.distinctBy { it.first }
+		.take(8)
+		.sortedBy { it.first }
+		.map { it.second }
+		.joinToString("")
 
 private val md5 = MessageDigest.getInstance("MD5")
 
-private fun hash(input: String, index: Int) = md5.let {
-	val hash = it.digest("$input$index".toByteArray())
-	it.reset()
-	DatatypeConverter.printHexBinary(hash)
-}
+private fun hash(input: String, index: Int) =
+		DatatypeConverter.printHexBinary(md5.digest("$input$index".toByteArray()))
 
 private fun String.parseHex() = Integer.parseInt(this, 16)
 
