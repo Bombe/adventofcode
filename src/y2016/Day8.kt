adb25257bdbeff11d479c74d5e7a4696cf9bde85
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-12-08 08:00:30 +0100

    Refactor day 8

--- src/y2016/Day8.kt
+++ src/y2016/Day8.kt
@@ -1,69 +1,68 @@
 package y2016
 
 fun main(args: Array<String>) {
-	println(first())
-	println(second())
+	println(first(BooleanArray(300, { false })))
+	println(second(BooleanArray(300, { false })))
 }
 
-private fun first() = getLines()
-		.map { "(rect|rotate row|rotate column)[^\\d]+([\\d]+)(?:x| by )([\\d]+)".toRegex().find(it)!!.groupValues.drop(1).let { Pair(it[0], Pair(it[1].toInt(), it[2].toInt())) } }
-		.fold(Screen()) { screen, instruction ->
-			screen.apply {
-				when (instruction.first) {
-					"rect" -> rect(instruction.second.first, instruction.second.second)
-					"rotate row" -> rotateRow(instruction.second.first, instruction.second.second)
-					"rotate column" -> rotateColumn(instruction.second.first, instruction.second.second)
-				}
-			}
-		}
+private val width = 50
+private val height = 6
+private val letterWidth = 5
+
+private fun first(screen: BooleanArray) = processScreen(screen)
+		.filter { it }
 		.count()
 
-class Screen {
-	private val width = 50
-	private val height = 6
-	private val letterWidth = 5
-	private val pixels = BooleanArray(width * height)
-	fun rect(a: Int, b: Int) {
-		(0..(a - 1)).forEach { column ->
-			(0..(b - 1)).forEach { row ->
-				pixels[row * width + column] = true
+private fun second(screen: BooleanArray) = processScreen(screen)
+		.display()
+
+private fun processScreen(screen: BooleanArray) = getLines()
+		.map { "(rect|rotate row|rotate column)[^\\d]+([\\d]+)(?:x| by )([\\d]+)".toRegex().find(it)!!.groupValues.drop(1).let { Pair(it[0], Pair(it[1].toInt(), it[2].toInt())) } }
+		.fold(screen.toList()) { screen, instruction ->
+			screen.let {
+				when (instruction.first) {
+					"rect" -> screen.rect(instruction.second.first, instruction.second.second)
+					"rotate row" -> screen.rotateRow(instruction.second.first, instruction.second.second)
+					"rotate column" -> screen.rotateColumn(instruction.second.first, instruction.second.second)
+					else -> throw IllegalArgumentException()
+				}
 			}
 		}
-	}
 
-	fun rotateColumn(column: Int, offset: Int) {
-		pixels
-				.filterIndexed { index, value -> (index % width) == column }
-				.forEachIndexed { index, value -> pixels[((index + offset) * width + column) % (width * height)] = value }
-	}
-
-	fun rotateRow(row: Int, offset: Int) {
-		pixels
-				.filterIndexed { index, value -> (index / width) == row }
-				.forEachIndexed { index, value -> pixels[row * width + ((index + offset) % width)] = value }
-	}
-
-	fun count() = pixels.count { it }
-
-	override fun toString() =
-			pixels.mapIndexed { index, value ->
-				(if (value) "\u2588" else " ").let {
-					it + (if (index % width == (width - 1)) "\n" else if (index % letterWidth == (letterWidth - 1)) " " else "")
-				}
-			}.joinToString("")
+fun List<Boolean>.rect(a: Int, b: Int) = mapIndexed { index, value ->
+	if ((index % width < a) && (index / width < b)) true else value
 }
 
-private fun second() = getLines()
-		.map { "(rect|rotate row|rotate column)[^\\d]+([\\d]+)(?:x| by )([\\d]+)".toRegex().find(it)!!.groupValues.drop(1).let { Pair(it[0], Pair(it[1].toInt(), it[2].toInt())) } }
-		.fold(Screen()) { screen, instruction ->
-			screen.apply {
-				when (instruction.first) {
-					"rect" -> rect(instruction.second.first, instruction.second.second)
-					"rotate row" -> rotateRow(instruction.second.first, instruction.second.second)
-					"rotate column" -> rotateColumn(instruction.second.first, instruction.second.second)
-				}
+fun List<Boolean>.rotateColumn(column: Int, offset: Int) = let {
+	it.filterIndexed { index, value -> (index % width) == column }.let { oldColumn ->
+		it.mapIndexed { index, value ->
+			if ((index % width) != column) {
+				value
+			} else {
+				oldColumn[(index / width + height - offset) % height]
 			}
 		}
+	}
+}
+
+fun List<Boolean>.rotateRow(row: Int, offset: Int) = let {
+	it.filterIndexed { index, value -> (index / width) == row }.let { oldRow ->
+		it.mapIndexed { index, value ->
+			if ((index / width) != row) {
+				value
+			} else {
+				oldRow[(index % width + width - offset) % width]
+			}
+		}
+	}
+}
+
+fun List<Boolean>.display() =
+		mapIndexed { index, value ->
+			(if (value) "\u2588" else " ").let {
+				it + (if (index % width == (width - 1)) "\n" else if (index % letterWidth == (letterWidth - 1)) " " else "")
+			}
+		}.joinToString("")
 
 private fun getLines(day: Int = 8) = AllDays().javaClass.getResourceAsStream("day$day.txt")
 		.reader()
