87818246533d6a06361cf56238518500769a9ec8
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-12-13 22:11:42 +0100

    Refactor solution for day 12

--- src/y2016/Day12.kt
+++ src/y2016/Day12.kt
@@ -5,8 +5,11 @@ fun main(args: Array<String>) {
 	println(second())
 }
 
-private fun first(): Int {
-	var cpu = Cpu()
+private fun first() = calculateA()
+private fun second() = calculateA(1)
+
+private fun calculateA(ignition: Int = 0): Int {
+	var cpu = Cpu(c = ignition)
 	val input = getLines()
 	while (cpu.ip < input.size) {
 		cpu = cpu.process(input[cpu.ip])
@@ -14,16 +17,11 @@ private fun first(): Int {
 	return cpu.a
 }
 
-private fun second(): Int {
-	var cpu = Cpu(c = 1)
-	val input = getLines()
-	while (cpu.ip < input.size) {
-		cpu = cpu.process(input[cpu.ip])
-	}
-	return cpu.a
-}
+private class Cpu private constructor(private val registers: Map<String, Int>, val ip: Int) {
 
-private data class Cpu(val a: Int = 0, val b: Int = 0, val c: Int = 0, val d: Int = 0, val ip: Int = 0) {
+	constructor(c: Int = 0) : this(mapOf("a" to 0, "b" to 0, "c" to c, "d" to 0), 0)
+
+	val a: Int get() = registers["a"]!!
 
 	fun process(command: String) = when {
 		command.startsWith("cpy") -> command.split(' ').let { cpy(it[1], it[2]) }
@@ -33,31 +31,11 @@ private data class Cpu(val a: Int = 0, val b: Int = 0, val c: Int = 0, val d: In
 		else -> throw IllegalArgumentException()
 	}
 
-	private fun cpy(source: String, register: String) = when (register) {
-		"a" -> copy(a = getValue(source), ip = ip + 1)
-		"b" -> copy(b = getValue(source), ip = ip + 1)
-		"c" -> copy(c = getValue(source), ip = ip + 1)
-		"d" -> copy(d = getValue(source), ip = ip + 1)
-		else -> throw IllegalArgumentException()
-	}
+	private fun cpy(source: String, register: String) = Cpu(registers + (register to getValue(source)), ip + 1)
+	private fun inc(register: String, increment: Int = 1) = Cpu(registers + (register to getValue(register) + increment), ip + 1)
+	private fun jnz(register: String, offset: Int) = Cpu(registers, ip + (if (getValue(register) != 0) offset else 1))
+	private fun getValue(source: String) = if (source in registers) registers[source]!! else source.toInt()
 
-	private fun inc(register: String, increment: Int = 1) = when (register) {
-		"a" -> copy(a = a + increment, ip = ip + 1)
-		"b" -> copy(b = b + increment, ip = ip + 1)
-		"c" -> copy(c = c + increment, ip = ip + 1)
-		"d" -> copy(d = d + increment, ip = ip + 1)
-		else -> throw IllegalArgumentException()
-	}
-
-	private fun jnz(register: String, offset: Int) = if (getValue(register) != 0) copy(ip = ip + offset) else copy(ip = ip + 1)
-
-	private fun getValue(source: String) = when (source) {
-		"a" -> a
-		"b" -> b
-		"c" -> c
-		"d" -> d
-		else -> source.toInt()
-	}
 }
 
 private fun getLines(day: Int = 12) = AllDays().javaClass.getResourceAsStream("day$day.txt")
