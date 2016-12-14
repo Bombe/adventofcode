f23a32098cf57458a12a75e805068a1cca48464f
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-12-14 07:41:18 +0100

    Refactor solution for day 13

--- src/y2016/Day13.kt
+++ src/y2016/Day13.kt
@@ -6,51 +6,45 @@ fun main(args: Array<String>) {
 }
 
 private fun first(): Int {
-	val map = (pairs().map { p ->
-		(p.first to p.second) to if (wall(p.first, p.second)) -1 else 0
-	} + ((1 to 1) to 1)).toMap(mutableMapOf())
+	val map = createMaze()
 	var counter = 0
 	while (map[31 to 39] == 0) {
 		counter++
-		pairs()
-				.filter { map[it] == counter }
-				.flatMap {
-					listOf(
-							it.first to (it.second - 1),
-							it.first to (it.second + 1),
-							(it.first - 1) to it.second,
-							(it.first + 1) to it.second)
-				}
-				.filterNot { it.first < 0 || it.second < 0 }
-				.filter { map[it] == 0 }
-				.distinct()
-				.forEach { map[it] = counter + 1 }
+		processMaze(map, counter)
 	}
 	return map[31 to 39]!! - 1
 }
 
-private fun pairs() = (0..40).flatMap { y -> (0..40).map { x -> (x to y) } }
+private fun createMaze(): MutableMap<Pair<Int, Int>, Int> {
+	return (0..40).flatMap { y ->
+		(0..40).map { x -> (x to y) }
+	}.map { p ->
+		(p.first to p.second) to if (wall(p.first, p.second)) -1 else if ((p.first == 1) && (p.second == 1)) 1 else 0
+	}.toMap(mutableMapOf())
+}
+
+private fun processMaze(map: MutableMap<Pair<Int, Int>, Int>, step: Int) {
+	map
+			.filter { it.value == step }
+			.flatMap {
+				listOf(
+						it.key.first to (it.key.second - 1),
+						it.key.first to (it.key.second + 1),
+						(it.key.first - 1) to it.key.second,
+						(it.key.first + 1) to it.key.second)
+			}
+			.filterNot { it.first < 0 || it.second < 0 }
+			.filter { map[it] == 0 }
+			.distinct()
+			.forEach { map[it] = step + 1 }
+}
 
 private fun second(): Int {
-	val map = (pairs().map { p ->
-		(p.first to p.second) to if (wall(p.first, p.second)) -1 else 0
-	} + ((1 to 1) to 1)).toMap(mutableMapOf())
+	val map = createMaze()
 	var counter = 0
 	while (counter < 50) {
 		counter++
-		pairs()
-				.filter { map[it] == counter }
-				.flatMap {
-					listOf(
-							it.first to (it.second - 1),
-							it.first to (it.second + 1),
-							(it.first - 1) to it.second,
-							(it.first + 1) to it.second)
-				}
-				.filterNot { it.first < 0 || it.second < 0 }
-				.filter { map[it] == 0 }
-				.distinct()
-				.forEach { map[it] = counter + 1 }
+		processMaze(map, counter)
 	}
 	return map.count { it.value > 0 }
 }
