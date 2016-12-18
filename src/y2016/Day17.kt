7d417671784469084f6740e1b832b3125d858aca
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-12-18 13:02:48 +0100

    Refactor solution for day 17

--- src/y2016/Day17.kt
+++ src/y2016/Day17.kt
@@ -12,30 +12,14 @@ private fun first() = solve().minBy { it.length }
 
 private fun second() = solve().map { it.length }.max()
 
-private fun solve(position: Int = 0, passcode: String = passcode(), pathsTaken: List<Set<Char>> = emptyList()): List<String> {
-	if (position == 15) {
-		return listOf(pathsTaken.map { it.first() }.joinToString(""))
-	} else {
-		return passcode.possibleDirections(pathsTaken.map { it.first() }.joinToString("")).filter { direction -> isPossible(position, direction) }.flatMap {
-			solve(position.move(it), passcode, pathsTaken.plus<Set<Char>>(setOf(it)))
+private fun solve(position: Int = 0, passcode: String = passcode(), pathsTaken: List<Set<Char>> = emptyList()): List<String> =
+		if (position == 15) {
+			listOf(pathsTaken.map { it.first() }.joinToString(""))
+		} else {
+			passcode.possibleDirections(pathsTaken.map { it.first() }.joinToString("")).filter { it.isPossible(position) }.flatMap {
+				solve(it.move(position), passcode, pathsTaken.plus<Set<Char>>(setOf(it.toChar())))
+			}
 		}
-	}
-}
-
-private fun Int.move(direction: Char) = when (direction) {
-	'U' -> this - 4
-	'D' -> this + 4
-	'L' -> this - 1
-	else -> this + 1
-}
-
-private fun isPossible(position: Int, direction: Char) =
-	when (direction) {
-		'U' -> position > 3
-		'D' -> position < 12
-		'L' -> (position % 4) > 0
-		else -> (position % 4) < 3
-	}
 
 private val md5 = MessageDigest.getInstance("MD5")
 private fun ByteArray.toHex() = DatatypeConverter.printHexBinary(this).toLowerCase()
@@ -45,12 +29,20 @@ private fun String.possibleDirections(path: String = "") = md5.digest((this + pa
 		.toCharArray()
 		.take(4)
 		.map { it > 'a' }
-		.let { mapOf(
-				'U' to it[0],
-				'D' to it[1],
-				'L' to it[2],
-				'R' to it[3]
-		)}.filterValues { it }
+		.zip(Direction.values())
+		.associate { it.second to it.first }
+		.filterValues { it }
 		.keys
+
+enum class Direction(private val offset: Int, private val check: (Int) -> Boolean) {
+	UP(-4, { it > 3 }),
+	DOWN(4, { it < 12 }),
+	LEFT(-1, { (it % 4) > 0 }),
+	RIGHT(1, { (it % 4) < 3 });
+
+	fun isPossible(position: Int) = check.invoke(position)
+	fun move(position: Int) = position + offset
+	fun toChar() = name[0]
+}
 
 private fun passcode() = "qtetzkpl"
