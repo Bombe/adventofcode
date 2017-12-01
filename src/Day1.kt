84583c9d323cf5ade5e355688a1c8e86c50a22ed
David Roden <d.roden@emetriq.com>
2017-12-01 09:25:27 +0100

    Generalize the solution

--- src/Day1.kt
+++ src/Day1.kt
@@ -7,23 +7,20 @@ private fun part1() =
 		readInput(1)
 				.first()
 				.map { it.toInt() - 48 }
-				.let { it + it[0] }
-				.let { input ->
-					input.drop(1).fold(0 to input[0]) { (sum, last), current ->
-						if (current == last) {
-							(sum + current) to current
-						} else {
-							sum to current
-						}
-					}.first
-				}
+				.solve(1)
 
 private fun part2() =
 		readInput(1)
 				.first()
 				.map { it.toInt() - 48 }
-				.let {
-					it.subList(0, it.size / 2).filterIndexed { index, digit ->
-						it.subList(it.size / 2, it.size)[index] == digit
-					}.sum() * 2
-				}
+				.let { it.solve(it.size / 2) }
+
+private fun List<Int>.solve(rotate: Int) =
+		rotate(rotate).let { rotated ->
+			filterIndexed { index, element ->
+				rotated[index] == element
+			}
+		}.sum()
+
+private fun <T> List<T>.rotate(steps: Int) =
+		subList(steps, size) + subList(0, steps)
