a87120bf4371795fce12f2843420609ef5584532
David Roden <d.roden@emetriq.com>
2017-12-11 10:40:01 +0100

    Move moving into a method on Coords

--- src/Day11.kt
+++ src/Day11.kt
@@ -9,32 +9,19 @@ fun main(args: Array<String>) {
 private fun getInput() = readInput(11).first().split(",")
 
 private fun part1(input: List<String> = getInput()) =
-		input.fold(Coords(0, 0, 0)) { coords, direction ->
-			when (direction) {
-				"n" -> coords.incX().decZ()
-				"s" -> coords.decX().incZ()
-				"ne" -> coords.incX().decY()
-				"sw" -> coords.decX().incY()
-				"nw" -> coords.incY().decZ()
-				"se" -> coords.decY().incZ()
-				else -> throw RuntimeException("invalid direction: $direction")
-			}
-		}.distance
+		input
+				.fold(Coords(), Coords::move)
+				.distance
 
 private fun part2(input: List<String> = getInput()) =
-		input.fold(Coords(0, 0, 0) to 0) { (coords, maxDistance), direction ->
-			when (direction) {
-				"n" -> coords.incX().decZ()
-				"s" -> coords.decX().incZ()
-				"ne" -> coords.incX().decY()
-				"sw" -> coords.decX().incY()
-				"nw" -> coords.incY().decZ()
-				"se" -> coords.decY().incZ()
-				else -> throw RuntimeException("invalid direction: $direction")
-			}.let { it to max(maxDistance, it.distance) }
-		}.second
+		input
+				.fold(Coords() to 0) { (coords, maxDistance), direction ->
+					coords
+							.move(direction)
+							.let { it to max(maxDistance, it.distance) }
+				}.second
 
-private data class Coords(val x: Int, val y: Int, val z: Int)
+private data class Coords(val x: Int = 0, val y: Int = 0, val z: Int = 0)
 
 private fun Coords.incX() = Coords(x + 1, y, z)
 private fun Coords.decX() = Coords(x - 1, y, z)
@@ -42,4 +29,14 @@ private fun Coords.incY() = Coords(x, y + 1, z)
 private fun Coords.decY() = Coords(x, y - 1, z)
 private fun Coords.incZ() = Coords(x, y, z + 1)
 private fun Coords.decZ() = Coords(x, y, z - 1)
+private fun Coords.move(direction: String) = when (direction) {
+	"n" -> incX().decZ()
+	"s" -> decX().incZ()
+	"ne" -> incX().decY()
+	"sw" -> decX().incY()
+	"nw" -> incY().decZ()
+	"se" -> decY().incZ()
+	else -> this
+}
+
 private val Coords.distance get() = (x.absoluteValue + y.absoluteValue + z.absoluteValue) / 2
