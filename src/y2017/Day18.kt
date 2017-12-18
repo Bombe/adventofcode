409b1d1c7f519dd1cc10af54e9d8c0b61dd37c29
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2017-12-18 07:36:36 +0100

    Refactor solution for day 18, part 1

--- src/y2017/Day18.kt
+++ src/y2017/Day18.kt
@@ -7,26 +7,31 @@ fun main(args: Array<String>) {
 private fun getInput() = readInput(18).toList()
 
 private fun part1(commands: List<String> = getInput()) =
-		generateSequence(DuetCpu()) { (registers, position, lastPlayedSound, lastReceivedSound) ->
+		generateSequence(DuetCpu()) { cpu ->
+			val (registers, position, lastPlayedSound, lastReceivedSound) = cpu
 			if ((position < 0) or (position >= commands.size) or (lastReceivedSound > -1)) {
 				null
 			} else {
 				commands[position].split(" ").let { parts ->
 					when (parts.first()) {
-						"snd" -> DuetCpu(registers, position + 1, getValue(registers, parts[1]), lastReceivedSound)
-						"set" -> DuetCpu(registers.apply { put(parts[1].first(), getValue(registers, parts[2])) }, position + 1, lastPlayedSound, lastReceivedSound)
-						"add" -> DuetCpu(registers.apply { put(parts[1].first(), getValue(registers, parts[1]) + getValue(registers, parts[2])) }, position + 1, lastPlayedSound, lastReceivedSound)
-						"mul" -> DuetCpu(registers.apply { put(parts[1].first(), getValue(registers, parts[1]) * getValue(registers, parts[2])) }, position + 1, lastPlayedSound, lastReceivedSound)
-						"mod" -> DuetCpu(registers.apply { put(parts[1].first(), getValue(registers, parts[1]) % getValue(registers, parts[2])) }, position + 1, lastPlayedSound, lastReceivedSound)
-						"rcv" -> DuetCpu(registers, position + 1, lastPlayedSound, if (getValue(registers, parts[1]) != 0L) lastPlayedSound else lastReceivedSound)
-						"jgz" -> DuetCpu(registers, if (getValue(registers, parts[1]) > 0) position + getValue(registers, parts[2]).toInt() else position + 1, lastPlayedSound, lastReceivedSound)
+						"snd" -> cpu.playSound(cpu[parts[1]]).advance()
+						"set" -> cpu.set(parts[1], cpu[parts[2]]).advance()
+						"add" -> cpu.set(parts[1], cpu[parts[1]] + cpu[parts[2]]).advance()
+						"mul" -> cpu.set(parts[1], cpu[parts[1]] * cpu[parts[2]]).advance()
+						"mod" -> cpu.set(parts[1], cpu[parts[1]] % cpu[parts[2]]).advance()
+						"rcv" -> cpu.recoverSound(parts[1]).advance()
+						"jgz" -> cpu.advance(if (cpu[parts[1]] > 0) cpu[parts[2]].toInt() else 1)
 						else -> throw RuntimeException("($registers,$position,$lastPlayedSound,$lastReceivedSound),$parts")
 					}
 				}
 			}
-		}.last().lastReceivedSound
+		}.last().lastRecoveredSound
 
-private data class DuetCpu(val registers: MutableMap<Char, Long> = mutableMapOf(), val position: Int = 0, val lastPlayedSound: Long = -1, val lastReceivedSound: Long = -1)
+private data class DuetCpu(val registers: MutableMap<Char, Long> = mutableMapOf(), val position: Int = 0, val lastPlayedSound: Long = -1, val lastRecoveredSound: Long = -1)
 
-private fun getValue(registers: Map<Char, Long>, value: String) =
-		value.toLongOrNull() ?: registers.getOrDefault(value.first(), 0)
+private fun DuetCpu.advance(steps: Int = 1) = copy(position = position + steps)
+private fun DuetCpu.playSound(value: Long) = copy(lastPlayedSound = value)
+private fun DuetCpu.recoverSound(registerOrValue: String) = if (this[registerOrValue] != 0L) copy(lastRecoveredSound = lastPlayedSound) else this
+private operator fun DuetCpu.get(registerOrValue: String) = registerOrValue.toLongOrNull() ?: registers.getOrDefault(registerOrValue.first(), 0)
+private operator fun DuetCpu.set(register: String, value: Long) = copy(registers = registers.apply { put(register.first(), value) })
+
