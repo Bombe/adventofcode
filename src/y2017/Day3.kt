733e0a936ff0b5505f7307114f99e702b3d883b4
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2017-12-20 11:15:25 +0100

    Add solution for day 3, part 1
    There won’t be a solution for part 2 from me.
    Look your value up on OEIS: https://oeis.org/A141481

---
+++ src/y2017/Day3.kt
@@ -0,0 +1,23 @@
+package y2017
+
+import kotlin.math.absoluteValue
+
+fun main(args: Array<String>) {
+	part1().println()
+}
+
+private fun getInput() = readInput(3).first().toInt()
+
+private fun part1(input: Int = getInput()) =
+		input.sqrt().let { squareRoot ->
+			(input - squareRoot * squareRoot).let { diff ->
+				when (diff) {
+					0 -> (squareRoot / 2) + (squareRoot / 2 - 1 + diff.odd())
+					in 1..squareRoot -> (squareRoot / 2 + diff.odd()) + (diff - (squareRoot / 2) - 1).absoluteValue
+					else -> (squareRoot / 2 + diff.odd()) + ((squareRoot + squareRoot / 2 + 1 + diff.odd()) - diff).absoluteValue
+				}
+			}
+		}
+
+private fun Int.odd() = this and 1
+private fun Int.sqrt() = kotlin.math.sqrt(toDouble()).toInt()
