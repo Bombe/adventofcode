8cefce903b00f25e8fca1eb29e378be3fbaab8f2
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2018-12-01 11:46:09 +0100

    Add solution for day 1

---
+++ src/y2018/Day1.kt
@@ -0,0 +1,21 @@
+package y2018
+
+fun main(args: Array<String>) {
+	part1().println()
+	part2()?.println()
+}
+
+fun part1() = readInput(1)
+		.map(Integer::parseInt)
+		.sum()
+
+private fun part2() = readInput(1).toList()
+		.loop()
+		.map(Integer::parseInt)
+		.foldWhile(0 to mutableSetOf<Int>()) { change, (currentFrequency, frequencies) ->
+			when {
+				currentFrequency + change in frequencies -> false to (currentFrequency + change to mutableSetOf())
+				else -> true to (currentFrequency + change to frequencies.apply { add(currentFrequency + change) })
+			}
+		}
+		?.first
---
+++ src/y2018/General.kt
@@ -0,0 +1,34 @@
+package y2018
+
+import java.util.stream.*
+
+fun Any.println() = println(this)
+
+fun readInput(day: Int): Sequence<String> {
+	class Foo
+	return Foo::class.java.getResourceAsStream("Input$day.txt").bufferedReader().lines().asSequence()
+}
+
+private fun <T> Stream<T>.asSequence(): Sequence<T> = Sequence { iterator() }
+
+fun <T> List<T>.loop(): Sequence<T> =
+		generateSequence(0) { index -> (index + 1) % size }
+				.map(this::get)
+
+fun <T, S> Sequence<T>.foldWhile(initialState: S, next: (T, S) -> Pair<Boolean, S>): S? {
+	var currentState = true to initialState
+	val iterator = iterator()
+	while (currentState.first && iterator.hasNext()) {
+		currentState = next(iterator.next(), currentState.second)
+	}
+	return currentState.second.takeUnless { currentState.first }
+}
+
+fun <T> List<T>.loop(n: Int): List<T> =
+		(0 until n).fold(mutableListOf()) { previous, _ ->
+			previous.apply { addAll(this@loop) }
+		}
+
+val Int.even get() = (this and 1) == 0
+val Int.sqr get() = this * this
+val Int.sqrt get() = Math.sqrt(toDouble()).toInt()
