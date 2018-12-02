b9efd2c8c5237c6002b9f927efa06c2aa2a9d301
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2018-12-02 06:36:33 +0100

    Add solution for day 2

---
+++ src/y2018/Day2.kt
@@ -0,0 +1,33 @@
+package y2018
+
+fun main(args: Array<String>) {
+	part1().println()
+	part2().println()
+}
+
+private fun part1() = readInput(2)
+		.map { it.toCharArray() }
+		.map { it.groupBy { it } }
+		.map { it.entries.map { (key, value) -> key to value.size } }
+		.map { it.any { it.second == 2 } to it.any { it.second == 3 } }
+		.fold(0 to 0) { (countTwos, countThrees), (foundTwo, foundThree) ->
+			(countTwos + if (foundTwo) 1 else 0) to (countThrees + if (foundThree) 1 else 0)
+		}
+		.let { it.first * it.second }
+
+
+private fun part2() = readInput(2).toList()
+		.let { input ->
+			input.flatMap { it.matchesCloselyOnIn(input) }
+		}
+		.distinct()
+
+private fun String.matchesCloselyOnIn(codes: List<String>) =
+		codes.map { findCommonLetters(it, this) }
+				.filter { it.size == (this.length - 1) }
+				.map { it.joinToString("") }
+
+private fun findCommonLetters(code: String, wantedCode: String) =
+		code.mapIndexedNotNull { index: Int, c: Char ->
+			c.takeIf { code[index] == wantedCode[index] }
+		}
