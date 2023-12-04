a57a7433f0e55b524990a35347375461f89a057c
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-12-04 20:58:37 +0100

    ♻️ Collectors.toList() creates new ArrayLists

--- src/y2015/Permutations.java
+++ src/y2015/Permutations.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -16,7 +15,7 @@ public class Permutations {
 
 	public static <V> List<List<V>> permutate(Collection<V> elements, Comparator<? super V> comparator) {
 		List<List<V>> allPermutations = new ArrayList<>();
-		List<V> currentPermutation = new ArrayList<>(elements.stream().sorted(comparator).collect(Collectors.toList()));
+		List<V> currentPermutation = elements.stream().sorted(comparator).collect(Collectors.toList());
 		// Pandita’s algorithm
 		while (true) {
 			// find largets k where a[k] < a [k+1]
