24f2615b21fc9aa87578f311f2c2b8a8a7081e45
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-12-04 22:24:44 +0100

    🚚 Move Java source files to correct path

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,4 +19,5 @@ dependencies {
 	implementation("org.junit.jupiter", "junit-jupiter-api", "5.10.1")
 	implementation("org.hamcrest", "hamcrest", "2.2")
 	implementation("javax.xml.bind", "jaxb-api", "2.3.1")
+	implementation("com.fasterxml.jackson.core", "jackson-databind", "2.16.0")
 }
